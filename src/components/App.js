9bac2a6e6cb06edd1f1d44439c1b2e0572f30411
Ashwini7Soni <soniashwini259@gmail.com>
2022-02-16 16:01:51 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,12 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <div className = "root">
+        <h1 data-ns-test = "project-name">Flipkart Clone</h1>
+        <p data-ns-test = "project-description">An app in react</p>
+      </div>
+    </div>
   )
 }
 
