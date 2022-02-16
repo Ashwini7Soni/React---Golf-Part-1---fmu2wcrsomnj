375570b0dd40b012ddf3879d01e19e633d1bc027
Ashwini7Soni <96107510+Ashwini7Soni@users.noreply.github.com>
2022-02-16 17:01:29 +0530

    third

--- src/components/App.js
+++ src/components/App.js
@@ -4,25 +4,33 @@ import "../styles/App.css";
 function App() {
   const [renderBall, setRenderBall] = useState(false);
   const [position, setPosition] = useState(0);
-  const [ballPosition, setBallPosition] = useState({ left: "0px" });
+  const [ballPosition, setBallPosition] = useState({ left: 0 });
 
-  
+  function updateBall(event){
+    if(event.key==="ArrowRight"){
+        setBallPosition(prevState=>({left:prevState.left+5}))
+    }
+}
+
+//Start Timer ---> clean Timer in cleanup
+// Add Event Listener ----> Remove Event Listener in Cleanup
+
 
   useEffect(() => {
-    document.addEventListener("keydown", function(event) {
-        if(event.key === "ArrowRight") {
-            setBallPosition((prevState) => {left: prevState.left+5})
-        }
-    });
+    document.addEventListener("keydown",updateBall)
+
+    return () => {
+        document.removeEventListener("keydown",updateBall)
+    }
   }, []);
 
   function buttonClickHandler() {
-      setRenderBall(true);
+     setRenderBall(true);
   }
 
   function renderBallOrButton() {
     if (renderBall) {
-      return <div className="ball" style={{position: "absolute", left: ballPosition.left}}></div>;
+      return <div className="ball" style={{position:"absolute",left:ballPosition.left}}></div>;
     } else {
       return <button onClick={buttonClickHandler}>Click For One Ball</button>;
     }
